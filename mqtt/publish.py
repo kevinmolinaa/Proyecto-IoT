4d2c6dfd59ff91e7824d099b0570bf1873f4f05e
Kevin Molina <kevinmolina.dev@gmail.com>
2023-05-24 13:05:19 -0400

    Mas mejoraras

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -1,4 +1,4 @@
 import paho.mqtt.publish as publish
 
-publish.single("boton_bool", "1", hostname="localhost")
-publish.single("valor_analog", "357", hostname="localhost")
+publish.single("boton_bool", "1", hostname="34.141.155.221")
+publish.single("valor_analog", "357", hostname="34.141.155.221")
