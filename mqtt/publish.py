f8992702ee674c5f3f5584b8aec93c8ed4853b30
Kevin Molina <kevinmolina.dev@gmail.com>
2023-05-24 15:13:33 -0400

    Mejoras con la conexion MQTT

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -1,4 +1,9 @@
 import paho.mqtt.publish as publish
 
-publish.single("boton_bool", "1", hostname="34.141.155.221")
-publish.single("valor_analog", "357", hostname="34.141.155.221")
+# Definir los valores de usuario y contraseña
+username = "kevin"
+password = "kevin"
+
+# Publicar mensajes con autenticación
+publish.single("boton_bool", "1", hostname="34.141.155.221", auth={'username': username, 'password': password})
+publish.single("valor_analog", "357", hostname="34.141.155.221", auth={'username': username, 'password': password})
--- mqtt/suscribe.py
+++ mqtt/suscribe.py
@@ -1,12 +1,17 @@
-#import context  
 import paho.mqtt.client as mqtt
 
 def on_message(mosq, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
+# Definir los valores de usuario y contraseña
+username = "kevin"
+password = "kevin"
+
 mqttc = mqtt.Client()
+mqttc.username_pw_set(username, password)  # Establecer usuario y contraseña
 mqttc.on_message = on_message
 mqttc.connect("34.141.155.221", 1883, 60)
 mqttc.subscribe("#", 0)
 
 mqttc.loop_forever()
+
