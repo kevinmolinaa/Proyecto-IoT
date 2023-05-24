4445a8a0f71f84c3ec8af486721092f14d7ac9ad
Kevin Molina <kevinmolina.dev@gmail.com>
2023-05-24 19:05:30 -0400

    Mejoras en MQTT

--- mqtt/publish.py
+++ mqtt/publish.py
@@ -4,7 +4,7 @@ import paho.mqtt.publish as publish
 # Definir los valores de usuario y contraseña
 username = "kevin"
 password = "kevin"
-
+mensaje=input("Ingrese el mensaje: ")
 # Publicar mensajes con autenticación
 publish.single("boton_bool", "1", hostname="34.141.155.221", auth={'username': username, 'password': password})
-publish.single("valor_analog",input("Ingrese el mensaje: "), hostname="34.141.155.221", auth={'username': username, 'password': password})
+publish.single("valor_analog",mensaje, hostname="34.141.155.221", auth={'username': username, 'password': password})
