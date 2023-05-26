230d73da623ee75b5a5e3efba57fc5a376b925c3
Kevin Molina <kevinmolina.dev@gmail.com>
2023-05-25 22:42:22 -0400

    Mejoras en el diseño y la API

--- class/db.php
+++ class/db.php
@@ -12,6 +12,7 @@
             try{
                 $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->database_name, $this->username, $this->password);
                 $this->conn->exec("set names utf8");
+                echo "Conexion Exitosa";
             }catch(PDOException $exception){
                 echo "No se pudo conectar a la base de datos: " . $exception->getMessage();
             }
